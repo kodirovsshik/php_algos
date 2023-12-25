72f90d242e5efb31624ca479615cde4e5a78ce17
kodirovsshik <20905994+kodirovsshik@users.noreply.github.com>
2023-12-25 18:03:58 +0300

    Add project files.

---
+++ 5_bm_search/index.php
@@ -0,0 +1,58 @@
+<?php
+
+function read($prompt)
+{
+	echo $prompt;
+	return readline();
+}
+
+function search_rightmost_char($s, $c, $i = 0)
+{
+    $n = strlen($s);
+    $top = $n - 1;
+
+	for (; $i < $top; ++$i)
+        if ($s[$top - $i] == $c)
+            break;
+    return min($i, $top);
+}
+
+function search($s, $sub)
+{
+	$n = strlen($s);
+	$nsub = strlen($sub);
+
+	$i = $nsub - 1;
+	$k = 0;
+	while ($i < $n) {
+		echo "$s\n";
+		echo str_repeat(' ', $i - ($nsub - 1)), "$sub\n";
+		echo str_repeat(' ', $i - $k), "^\n";
+
+		$c = $s[$i - $k];
+		if ($c == $sub[$nsub - 1 - $k])
+        {
+			$k++;
+			if ($k != $nsub)
+				continue;
+
+			echo "Подстрока найдена на позиции ", $i - $nsub + 1, "\n";
+            $k = 0;
+            ++$i;
+		}
+		else
+        {
+			$i += search_rightmost_char($sub, $c, $k);
+            $k = 0;
+		}
+	}
+}
+
+
+$s = read("Введите строку: ");
+$sub = read("Введите подстроку: ");
+
+search($s, $sub);
+
+echo 1;
+
