c8a9501f92a69793c42cc5ac5f3b3d07e6018dfd
kodirovsshik <20905994+kodirovsshik@users.noreply.github.com>
2024-01-07 23:37:38 +0300

    actual string trie

--- 11_bin_trie/index.php
+++ 11_bin_trie/index.php
@@ -10,158 +10,83 @@ function swap(&$x, &$y)
 class node
 {
 	public $val;
-	public $left, $right;
+	public $next;
 
-	public function __construct($val = NAN)
+	public function __construct($val = false)
 	{
 		$this->val = $val;
+		$this->next = array();
 	}
-
-	public function __toString()
-	{
-		$result = "";
-
-		$result .= "[";
-		$result .= $this->val;
-		$result .= ", ";
-		$result .= nodeToStr($this->right);
-		$result .= ", ";
-		$result .= nodeToStr($this->left);
-		$result .= "]";
-
-		return $result;
-	}
-}
-
-function nodeToStr($n)
-{
-	if (is_null($n))
-		return "[]";
-	return (string) $n;
 }
 
 function traverse_tree($root, $callable, $level = 0)
 {
 	if (is_null($root))
 		return;
-	traverse_tree($root->right, $callable, $level + 1);
 	$callable($root, $level);
-	traverse_tree($root->left, $callable, $level + 1);
+	foreach ($root->next as $n)
+		traverse_tree($n, $callable, $level + 1);
 }
 
-function print_trie($root, $l = 0)
+function print_trie($root, $l = 0, $s = "")
 {
-    traverse_tree(
-        $root,
-        function ($n, $l) {
-            $x = $n->val;
-            if ($x === null)
-                $x = "x";
-            echo str_repeat("    ", $l);
-            echo $x, "\n";
-        }
-    );
-    return;
-
-    if ($root === null)
-        return;
-
-    print_trie($root->right, $l + 1);
-	if ($root->val !== null)
-    {
-        echo str_repeat("    ", $l);
-        echo $root->val, "\n";
-    }
-    print_trie($root->left, $l + 1);
-}
-
-function insert_trie(&$root, $val, $bits = null)
-{
-	if ($bits === null)
-		$bits = $val;
-
-    if ($root === null)
-		$root = new node(null);
-
-	if ($bits == 0)
-    {
-		$root->val = $val;
+	if ($root === null)
 		return;
-    }
+	if ($root->val)
+		echo str_repeat("    ", $l), $s, "\n";
+	foreach ($root->next as $c => $n)
+        print_trie($n, $l + 1, $s . $c);
+}
 
-    $newbits = $bits >> 1;
-	if ($bits & 1)
-		insert_trie($root->right, $val, $newbits);
+function insert_trie(&$root, $val, $l = 0)
+{
+	if ($root === null)
+		$root = new node();
+	if ($l == strlen($val))
+		$root->val = true;
 	else
-		insert_trie($root->left, $val, $newbits);
+		insert_trie($root->next[$val[$l]], $val, $l + 1);
 }
-
-function count_tree_pred($root, $callable)
-{
-	$c = 0;
-	traverse_tree(
-		$root,
-		function ($n, $l) use (&$c, $callable) {
-			$c += $callable($n, $l);
-		}
-	);
-	return $c;
-}
-function tree_size($root)
-{
-	return count_tree_pred($root, function ($n, $l) {
-		return 1;
-	});
-}
-function search_tree($root, $val)
+function search_trie($root, $val, $l = 0)
 {
 	if (is_null($root))
 		return false;
-	if ($root->val == $val)
-		return true;
-	if ($val < $root->val)
-		return search_tree($root->left, $val);
-	else
-		return search_tree($root->right, $val);
+	if ($l == strlen($val))
+		return (bool)$root->val;
+	return search_trie($root->next[$val[$l]], $val, $l + 1);
 }
-function delete_trie_val(&$root, $val)
+function delete_trie_val(&$root, $val, $l = 0)
+{
+	if ($root === null)
+		return;
+	if ($l == strlen($val))
+		$root->val = false;
+	else
+		delete_trie_val($root->next[$val[$l]], $val, $l + 1);
+}
+function delete_trie(&$root)
 {
 	if (is_null($root))
 		return;
-
-	if ($root->val == $val) {
-        $root->val = null;
-        return;
-	}
-
-	if ($val < $root->val)
-		delete_trie_val($root->left, $val);
-	else
-		delete_trie_val($root->right, $val);
-}
-function delete_tree(&$root)
-{
-	if (is_null($root))
-		return;
-	delete_tree($root->left);
-	delete_tree($root->right);
+	foreach ($root->next as $n)
+		delete_trie($n);
 	$root = null;
 }
 
 
 
-$tree = null;
-$vals = [3, 2, 7, 5, 4, 8, 9, 6];
-$deletee = 7;
+$tree = new node();
+$vals = ["abcd", "abce", "a", "ac", "ab"];
+$deletee = "ab";
 
 $tree_info = function () use (&$tree, $deletee) {
 	print_trie($tree);
-	echo "Поиск значения $deletee: ", (int) search_tree($tree, $deletee), "\n";
+	echo "Поиск значения $deletee: ", (int) search_trie($tree, $deletee), "\n";
 };
 
 
 
-echo "Создание поразрадного дерева\n\n";
+echo "Создание бора\n\n";
 foreach ($vals as $val)
 	insert_trie($tree, $val);
 
@@ -169,15 +94,15 @@ $tree_info();
 
 
 
-echo "\nУдаление элемента $deletee\n\n";
+echo "\nУдаление элемента \"$deletee\"\n\n";
 delete_trie_val($tree, $deletee);
 
 $tree_info();
 
 
 
-echo "\nУдаление дерева\n\n";
-delete_tree($tree);
+echo "\nУдаление бора\n\n";
+delete_trie($tree);
 
 $tree_info();
 
